63da4da28f4f1b9af28aa258980d7bcf2c4b4b20
VidameSamwell <42365807+samwell9624@users.noreply.github.com>
2024-04-18 16:54:44 +0800

    upload

--- js/script.js
+++ js/script.js
@@ -1,56 +1,92 @@
-function toggleClass(selector, className) {
-    const element = document.querySelector(selector);
-    element.addEventListener("click", () => {
-        element.classList.toggle(className);
-    });
+// logo-menu on hover or click show sidebar
+const logoMenu = document.getElementById("logo-menu");
+logoMenu.addEventListener("click", () => {
+    logoMenu.classList.toggle("logo-menu-open");
+});
+logoMenu.addEventListener("mouseover", () => {
+    logoMenu.classList.toggle("logo-menu-open");
+});
+
+// dark mode button
+const darkModeButton = document.getElementById("dark-mode-button");
+darkModeButton.addEventListener("click", changeTheme);
+
+// change theme site css to dark mode
+function changeTheme() {
+    const darkModeButton = document.getElementById("dark-mode-button");
+    if (darkModeButton.innerHTML === "Dark Mode") {
+        darkModeButton.innerHTML = "Light Mode";
+        document.getElementById('theme-icon').innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                 class="bi bi-moon-stars-fill" viewBox="0 0 16 16" id="theme-icon">
+                <path
+                    d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z"/>
+                <path
+                    d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.734 1.734 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.734 1.734 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.734 1.734 0 0 0 1.097-1.097l.387-1.162zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L13.863.1z"/>
+            </svg>`;
+    } else {
+        darkModeButton.innerHTML = "Dark Mode";
+        document.body.classList.remove("dark-theme");
+        document.getElementById('theme-icon').innerHTML =
+            `<svg xmlns="http://www.w3.org/2000/svg" id="theme-icon" width="16" height="16" fill="currentColor"
+class="bi bi-sun" 
+viewBox="0 0 16 16">
+  <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6m0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708"/>
+  </svg>`;
+        document.body.classList.add("dark-theme");
+    }
 }
 
-toggleClass(".right-toolbar-button", "show");
-toggleClass(".left-toolbar-button", "show");
-toggleClass(".menu-button", "show");
+// detect firefox
+function detectFirefox() {
+    const isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+    if (isFirefox) {
+        const notification = document.createElement('div');
+        notification.textContent = notificationLanguage[language].notificationFirefox;
+        notification.style.fontSize = '1.5rem';
+        notification.style.fontWeight = 'bold';
+        notification.style.backgroundColor = '#ffcccc';
+        notification.style.color = '#ff0000';
+        notification.style.padding = '10px';
+        notification.style.position = 'fixed';
+        notification.style.top = '0';
+        notification.style.left = '0';
+        notification.style.width = '100%';
+        notification.style.textAlign = 'center';
+        document.body.appendChild(notification);
+        const hideDiv = document.getElementById('Not-show-on-firefox');
+        window.onload = function () {
+            hideDiv.style.display = 'block';
+        }
+    }
+}
 
-// add navbar menu to html
-function addNavbarMenuToHtml() {
-    const navbarMenuHtml = document.createElement("div");
-    navbarMenuHtml.classList.add("navbar-menu");
-    navbarMenuHtml.innerHTML = `
-        <div class="navbar-menu-div">
-        <button class="navbar-home-button">
-        <a href="index.html">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-             class="bi bi-house" viewBox="0 0 16 16">
-            <path d="M4 13.5V2H2v3H0v9h3v3h13v-3h3v-9h-3zM6.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-1 0V3.5a.5.5 0 0 1 .5-.5zm0 1a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 1 0V3.5a.5.5 0 0 0-.5-.5z"/>
-            <path d="M10.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-1 0V3.5a.5.5 0 0 1 .5-.5zm0 1a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 1 0V3.5a.5.5 0 0 0-.5-.5z"/>
-            <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-1 0V3.5a.5.5 0 0 1 .5-.5zm0 1a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 1 0V3.5a.5.5 0 0 0-.5-.5z"/>
-        </svg>
-        </a>
-        <button class="navbar-menu-button">
-        <a href="menu.html">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-             class="bi bi-list" viewBox="0 0 16 16">
-            <path d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-        </svg>
-        </a>
-        </button>
-        <button class="navbar-about-button">
-        <a href="about.html">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-             class="bi bi-person-circle" viewBox="0 0 16 16">
-            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-            <path d="M5.25 5.5a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3z"/>
-            <path d="M2.5 2.5A.5.5 0 0 1 3 2h8a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0 4A.5.5 0 0 1 3 7h8a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0 4A.5.5 0 0 1 3 12h8a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-        </svg>
-        </a>
-        </button>
-        <button class="navbar-menu-contact-button">
-        <a href="contact.html">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-             class="bi bi-envelope" viewBox="0 0 16 16">
-            <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1H2z"/>
-        </svg>
-        </a>
-        </button>
-        </div>
-        `;
-    navbarMenuHtml.classList.add("navbar-menu");
+// notification language map
+const notificationLanguage = {
+    en: {
+        notification: 'Notification',
+        notificationFirefox: 'We are sorry, but this website is not supported in Firefox. Please use Chrome or Safari instead.'
+    },
+    "zh-hant": {
+        notification: '通知',
+        notificationFirefox: '我們很抱歉，這個網站不支援 Firefox。請使用 Chrome 或 Safari 代替。'
+    },
+    "zh-hans": {
+        notification: '通知',
+        notificationFirefox: '我们很抱歉，这个网站不支持 Firefox。请使用 Chrome 或 Safari 代替。'
+    },
+    ja: {
+        notification: '通知',
+        notificationFirefox: '申し訳ありませんが、このウェブサイトは Firefox ではサポートされていません。Chrome または Safari を使用してください。'
+    }
+}
+
+function changeLanguage(language) {
+    const translations = languageMap[language];
+    for (const key in translations) {
+        const element = document.getElementById(key);
+        if (element) {
+            element.textContent = translations[key];
+        }
+    }
 }
